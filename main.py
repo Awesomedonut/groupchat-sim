219d67cf450d8c1bf6a26cb71d855d5c5b1af737
Awesomedonut <rainyjsong@gmail.com>
2024-11-07 09:49:43 -0800

    update

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from chat_gui import ChatGUI
 
 if __name__ == "__main__":
     root = tk.Tk()
-    # print here???
+    print("hi")
     app = ChatGUI(root)
     root.mainloop()
     print("program running! :D") #ok this only ran when i pressed exit?????
 
-# todo: have option to select character or unselect character for scene
+# todo: have option to select character or unselect character for scene -- bot is actually pretty smart here, preliminary testing shows the one thats spoken to responds
 # change context? or look at context ai has
 # summarize when context window is filled
 # prettify ui
+# add history?
